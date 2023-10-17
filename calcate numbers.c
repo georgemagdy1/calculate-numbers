9f017ed985208b27b4da58543b54cdeaa5637cc4
George magdy <117531336+georgemagdy1@users.noreply.github.com>
2023-10-17 19:25:17 +0300

    Update calcate numbers.c

--- calcate numbers.c
+++ calcate numbers.c
@@ -7,10 +7,11 @@ int main()
 	char op;
 	printf("enter your first number");
 	scanf("%d", &x);
-	printf("enter your second number");
-	scanf("%d", &y);
 	printf("enter your op");
 	scanf(" %c", &op);
+	printf("enter your second number");
+	scanf("%d", &y);
+
 	if (op == '+') {
 
 		printf("your result %d+%d=%d", x, y, x + y);
